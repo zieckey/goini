8522b2d9edbd931eeec27f0361d6b804f399559a
weizili.360.laptop <zieckey@gmail.com>
2014-11-18 11:10:01 +0800

    Add examples

---
+++ examples/inifileparsing/main.go
@@ -0,0 +1,60 @@
+// Copyright 2014 zieckey. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+
+	"github.com/zieckey/wini"
+)
+
+func main() {
+	filename := filepath.Join(testDataDir(), "ini_parser_testfile.ini")
+	ini := wini.New()
+	err := ini.ParseFile(filename)
+	if err != nil {
+		fmt.Printf("parse INI file %v failed : %v\n", filename, err.Error())
+		return
+	}
+
+	v, ok := ini.Get("mid")
+	if !ok || v != "ac9219aa5232c4e519ae5fcb4d77ae5b" {
+		fmt.Printf("mid value is invalid\n")
+		return
+	}
+	fmt.Printf("the value of [mid] in INI is [%v] ok=[%v]\n", v, ok)
+
+	sss, ok := ini.GetKvmap("sss")
+	size := len(sss)
+	if size != 2 {
+		fmt.Printf("sss size is invalid\n")
+		return
+	}
+	
+	v, ok = ini.SectionGet("sss", "aa")
+	if !ok || v != "bb" {
+		fmt.Printf("sss/aa value is invalid\n")
+		return
+	}
+	
+	v, ok = ini.SectionGet("sss", "appext")
+	if !ok || v != "ab=cd" {
+		fmt.Printf("sss/appext value is invalid\n")
+		return
+	}
+}
+
+func testDataDir() string {
+	var file string
+	var ok bool
+	if _, file, _, ok = runtime.Caller(0); ok {
+		curdir := filepath.Dir(file)
+		return filepath.Join(curdir, "../data")
+	}
+
+	return ""
+}
---
+++ examples/memoryiniparsing/main.go
@@ -0,0 +1,32 @@
+// Copyright 2014 zieckey. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"github.com/zieckey/wini"
+)
+
+func main() {
+	raw := []byte("a:av|b:bv||c:cv|||d:dv||||||")
+	ini := wini.New()
+	err := ini.Parse(raw, "|", ":")
+	if err != nil {
+		fmt.Printf("parse INI memory data failed : %v\n", err.Error())
+		return
+	}
+
+	key := "a"
+	v, ok := ini.Get(key)
+	if ok {
+		fmt.Printf("The value of %v is [%v]\n", key, v) // Output : The value of a is [av]
+	}
+
+	key = "c"
+	v, ok = ini.Get(key)
+	if ok {
+		fmt.Printf("The value of %v is [%v]\n", key, v) // Output : The value of c is [cv]
+	}
+}
